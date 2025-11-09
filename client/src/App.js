8d611a88e19429b7a6f82816c03abc43d67f6778
saheerkumble <safsaheer@gmail.com>
2025-11-09 08:54:25 +0530

    Front end and back end connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [serverMessage, setServerMessage] = useState("Connecting...");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/ping")
+      .then(res => setServerMessage(res.data.message))
+      .catch(() => setServerMessage("Failed to connect to server"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
+      <h2>{serverMessage}</h2>
     </div>
   );
 }
